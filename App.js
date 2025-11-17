ce89bfea969cd881a467c4182c9382f67042959c
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2025-11-17 15:19:33 +0000

    Recording bug

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useAudioRecorder, RecordingPresets } from "expo-audio";
+
+const options = {
+  ...RecordingPresets.HIGH_QUALITY,
+  web: {
+    mimeType: "audio/webm",
+    bitsPerSecond: 96000,
+  },
+};
 
 export default function App() {
+  const recorder = useAudioRecorder(options);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Expected: {JSON.stringify(options.web)}</Text>
+      <Text>Actual: {JSON.stringify(recorder.options)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
